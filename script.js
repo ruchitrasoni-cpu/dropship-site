6e7505d470f7a0ec5a6ffae453e30a48585d2c5f
ruchitrasoni-cpu <ruchitrasoni@gmail.com>
2026-03-01 17:01:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const productList = document.getElementById("product-list");
+const products = JSON.parse(localStorage.getItem("products")) || [];
+
+products.forEach(p => {
+  productList.innerHTML += `
+    <div class="product">
+      <img src="${p.image}">
+      <h3>${p.name}</h3>
+      <p class="price">${p.price}</p>
+      <button onclick="orderNow('${p.name}')">Order on WhatsApp</button>
+    </div>
+  `;
+});
+
+function orderNow(name) {
+  const phone = "91XXXXXXXXXX"; // apna number yahan dalo
+  const msg = `Hello, I want to order ${name}`;
+  window.open(`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`);
+}
