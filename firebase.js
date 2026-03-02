e8391939298bf5d5e28ded7ea3bf71a392ab5282
ruchitrasoni-cpu <ruchitrasoni@gmail.com>
2026-03-02 10:55:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js (GitHub Pages compatible)
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBiBKmdQtZSKbMOlaRNBrWdCcstXs4XGY0",
-  authDomain: "dropship-store-16f41.firebaseapp.com",
-  projectId: "dropship-store-16f41",
-  storageBucket: "dropship-store-16f41.firebasestorage.app",
-  messagingSenderId: "925356783415",
-  appId: "1:925356783415:web:11a7d4315282b44c5b4cb2",
-  measurementId: "G-48563G7G20"
+  apiKey: "PASTE_YOUR_API_KEY",
+  authDomain: "PASTE_YOUR_AUTH_DOMAIN",
+  projectId: "PASTE_YOUR_PROJECT_ID",
+  storageBucket: "PASTE_YOUR_STORAGE_BUCKET",
+  messagingSenderId: "PASTE_YOUR_SENDER_ID",
+  appId: "PASTE_YOUR_APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
