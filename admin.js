eaaaab15b54a9fce903afe3267ebeba15d8bd83a
ruchitrasoni-cpu <ruchitrasoni@gmail.com>
2026-03-02 12:11:49 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,9 +1,23 @@
 import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-console.log("firebase db object:", db);
+console.log("admin js + firebase loaded");
 
 const btn = document.getElementById("addBtn");
 
-btn.addEventListener("click", () => {
-  alert("Firebase connected, next step DB write");
+btn.addEventListener("click", async () => {
+  try {
+    await addDoc(collection(db, "products"), {
+      name: "Test Product",
+      price: "999",
+      image: "https://via.placeholder.com/300"
+    });
+    alert("PRODUCT ADDED TO FIREBASE");
+  } catch (e) {
+    console.error("Firebase error:", e);
+    alert("ERROR: " + e.message);
+  }
 });
